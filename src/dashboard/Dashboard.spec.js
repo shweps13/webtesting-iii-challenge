f314fabafaeb692ecf9fab131edf5139e440f7ad
Heorhii Siburov <g.siburov@gmail.com>
2019-09-16 14:54:18 -0700

    test: shows the controls and display

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,24 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Dashboard from './Dashboard';
+import { Display } from '../display/Display';
+import { Controls } from '../controls/Controls';
+
+test('Dashboard renders without crashing', () => {
+  render(<Dashboard />);
+});
+
+test('Display element showings on Dashboard page', () => {
+
+    const { getByTestId } = render(<Dashboard />);
+    getByTestId(/display-element/i);
+  });
+ 
+test('Controls element showings on Dashboard page', () => {
+
+    const { getByTestId } = render(<Dashboard />);
+    getByTestId(/controls-element/i);
+});
