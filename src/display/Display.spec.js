82918cd19ff28431191e6c980452cb32368f01c2
Heorhii Siburov <g.siburov@gmail.com>
2019-09-16 17:28:11 -0700

    Some test Display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,43 @@
-// Test away!
+// Test away!// Test away
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import renderer from "react-test-renderer";
+
+import Display from './Display';
+
+
+test('Dashboard renders without crashing', () => {
+  render(<Display />);
+});
+
+test('Matches snapshot', () => {
+    const tree = renderer.create(<Display />); 
+    expect(tree.toJSON()).toMatchSnapshot();
+});
+
+test('Display [open/unlocked] by default', () => {
+    const { queryByText } = render(<Display />);
+    expect(queryByText(/Open/i)).toBeTruthy();
+    expect(queryByText(/Unlocked/i)).toBeTruthy();
+})
+
+test('Display "Closed" if closed prop is true', () => {
+    const { getByText } = render(<Display closed={true} />);
+    expect(getByText('Closed'));
+})
+
+test('Display "Locked" if locked prop is true', () => {
+    const { getByText } = render(<Display locked={true} />);
+    expect(getByText('Locked'));
+})
+
+test('Displays "Open" if closed prop is false', () => {
+    const { getByText } = render(<Display closed={false} />);
+    getByText(/open/i);
+});
+
+test('Displays "Unlock" if locked prop is false', () => {
+    const { getByText } = render(<Display locked={false} />);
+    getByText(/unlocked/i);
+});
